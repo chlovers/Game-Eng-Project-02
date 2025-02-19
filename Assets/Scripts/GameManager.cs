b165fb59fb1a3fd92c6a082ede8933dfbc0a5818
chlovers <chloie_day@hotmail.com>
2025-02-19 15:11:34 -0400

    added scripts and updated game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    // Start is called before the first frame update
+   
 
     public  LevelManager levelManager;
-  
+    public GameStateManager gameState;
+    public UIManager iManager;
+
 
     void Start()
     {

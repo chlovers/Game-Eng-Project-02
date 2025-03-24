eeab07bc0a28721353f92c37433dc8dc3039d475
chlovers <chloie_day@hotmail.com>
2025-03-24 08:46:15 -0300

    new project

---
+++ Assets/Scripts/Interaction object.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactionobject : MonoBehaviour
+{
+public void inter()
+    {
+        Debug.Log("found the" + gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Object interact.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectinteract : MonoBehaviour
+{
+   public GameObject obj;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && obj !=null)
+        {
+            obj.GetComponent<Interactionobject>().inter();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pickup"))
+        {
+            obj = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pickup"))
+        {
+            obj = null;
+        }
+
+        
+    }
+}
